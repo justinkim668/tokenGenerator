c5b129a2e9f158c856f293f32fde11a0def1b1a9
justinkim668 <jtk2141@columbia.edu>
2021-07-13 14:11:29 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+class TokenSquare{
+  const(number){
+    this.number = number;
+  }
+}
+
+function getRandom(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function generateHTMLSquares(){
+  const numberOfSquares = 8;
+  let squaresHTML = '';
+  for (let i = 0; i < numberOfSquares; i++){
+    squaresHTML +=
+    '<div class = "board-square">' +
+    '<h1 id = "token-number" class = "text-center">'+getRandom(0,10)+'</h1>' +
+    '</div>';
+  }
+  const tokenElement = document.getElementById('token-field');
+  tokenElement.innerHTML = squaresHTML;
+
+}
+
+var number = setInterval(generateHTMLSquares, 1000);
+
+//generateHTMLSquares();
